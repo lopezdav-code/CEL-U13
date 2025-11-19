ac86211dd62a8061560b951e5c356c6c8b69cac8
lopezdav-code <lopez.dav@gmail.com>
2025-11-19 18:22:47 +0100

    chore: Add Cypress projectId to config

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'cypress';
 
 export default defineConfig({
+    projectId: "a3zdtp",
     e2e: {
         baseUrl: "http://localhost:3000",
         supportFile: "cypress/support/e2e.js",
